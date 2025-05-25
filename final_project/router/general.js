2dea7ef0d545b0cddbca9b11b15d85630300ec7f
x-continuumlabs.io <dev@continuumlabs.io>
2025-05-25 22:00:20 +0200

    Add async to '/' route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -26,8 +27,22 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-	res.status(200).json(books);
+public_users.get('/', async function (req, res) {
+	try {
+		// Simulate server request with 2 second timeout
+		await new Promise(resolve => setTimeout(resolve, 2000));
+		
+		// Simulate axios request (since we're simulating, we'll just return the books data)
+		const response = await new Promise((resolve) => {
+			setTimeout(() => {
+				resolve({ data: books });
+			}, 0);
+		});
+		
+		res.status(200).json(response.data);
+	} catch (error) {
+		res.status(500).json({ message: "Error fetching books" });
+	}
 });
 
 // Get book details based on ISBN
