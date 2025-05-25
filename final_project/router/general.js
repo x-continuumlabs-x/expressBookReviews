55aa70780fd368dcbe2c56c3a64dc03ee7d77588
x-continuumlabs.io <dev@continuumlabs.io>
2025-05-25 20:13:27 +0200

    Add get author logic

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,21 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	const author = req.params.author;
+	const bookKeys = Object.keys(books);
+	const booksByAuthor = [];
+
+	for (let key of bookKeys) {
+		if (books[key].author === author) {
+			booksByAuthor.push(books[key]);
+		}
+	}
+
+	if (booksByAuthor.length > 0) {
+		return res.status(200).json(booksByAuthor);
+	} else {
+		return res.status(404).json({message: "No books found by this author"});
+	}
 });
 
 // Get all books based on title
