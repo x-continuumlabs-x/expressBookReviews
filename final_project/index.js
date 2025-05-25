ec30f2d6fb486331fbe832ffb23254b58ab6de25
x-continuumlabs.io <dev@continuumlabs.io>
2025-05-25 18:04:43 +0200

    Update authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,19 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session && req.session.accessToken) {
+        jwt.verify(req.session.accessToken, "fingerprint_customer", (err, decoded) => {
+            if (err) {
+                return res.status(401).json({message: "Unauthorized. Invalid or expired token"});
+            } else {
+                req.user = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(401).json({message: "Unauthorized. No access token found"});
+    }
 });
  
 const PORT =5000;
